a06f52b3043b22bb5b0bd6759b067a225557c070
Andi-Liu <38970640+Andi-Liu@users.noreply.github.com>
2019-03-12 20:19:05 +0800

    Add files via upload

--- array.js
+++ array.js
@@ -2,8 +2,11 @@
 
 
 var myArray = [10,20,30,40,50];
+console.log(myArray);
 
-myArray.splice(3,0,1,3,5,7);
+
+//remove first item in array
+myArray.shift();
 
 console.log(myArray);
 console.log(myArray.length);
